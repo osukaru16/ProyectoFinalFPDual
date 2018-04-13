48b3626b47a19495bdb3dad3ac1a14505471e96f
osukaru16 <osukaru16@gmail.com>
2018-04-13 16:11:30 +0200

    Add files via upload

--- -                    proyecto/js/heptagrama.js
+++ -                    proyecto/js/heptagrama.js
@@ -4,6 +4,8 @@
 
 var canvas = SVG('heptagrama').size('100%', '100%').viewbox(0,0,800,1000);
 
+//var listaNotasCreadas = [];
+
 /*
 
 	Nota: he comprobado que stroke-width:10; en muy grande usando el SVG.js
@@ -24,11 +26,30 @@ var canvas = SVG('heptagrama').size('100%', '100%').viewbox(0,0,800,1000);
 //<text x="30" y="50">B#4</text>
 
 pintaHeptagrama(canvas);
+dibujaNota(canvas, 1, "4", true);
+dibujaNota(canvas, 2, "4", true);
+dibujaNota(canvas, 3, "4", true);
+dibujaNota(canvas, 4, "4", true);
+dibujaNota(canvas, 5, "4", true);
+dibujaNota(canvas, 6, "4", true);
+dibujaNota(canvas, 7, "4", true);
+
+
+
+/*
+canvas.circle(30).attr({ cx: 198, cy: 50 }).fill("#022486");
+canvas.text("4").move(190, 33).font({ fill: "#ffffff", family: 'Inconsolata'});
+//canvas.circle(5).attr({ cx: 198, cy: 50 }).fill("#b3b300");
+*/
+
+
+
 
 function pintaHeptagrama(canvas){
 
 	var notasArray = ["B", "A", "G", "F", "E", "D", "C"];
 	var colorLetras = ["#022486", "#402284", "#02948A", "#00A900", "#FFEE00", "#FF491A", "#F50417"];
+	var posicionNotas = [50, 100, 150, 200, 250, 300, 350];
 
 	canvas.line(10, 10, 10, 380);
 	canvas.line(70, 10, 70, 380);
@@ -39,18 +60,20 @@ function pintaHeptagrama(canvas){
 
 
 
-	var posicionesY = 50;
-	var posucionLetrasY = 30;
+	//var posicionesY = 50;
+	var posicionLetrasY = 30;
 
 	for (var i = 0; i < 7; i++) {
 
-		canvas.line(70, posicionesY, 1000, posicionesY);
-		canvas.circle(30).attr({ cx: 100, cy: posicionesY }).fill('#bfbfbf');
-		canvas.circle(5).attr({ cx: 100, cy: posicionesY }).fill('#ffffff');
-		canvas.text(notasArray[i]).move(30, posucionLetrasY).fill('#bfbfbf').font({ fill: colorLetras[i], family: 'Inconsolata' });
+		var posicionesY = posicionNotas[i];
 
-		posucionLetrasY += 50;
-		posicionesY += 50;
+		canvas.line(70, posicionesY, 1000, posicionesY);
+		canvas.circle(30).attr({ cx: 100, cy: posicionesY }).fill('#bfbfbf').stroke("white");
+		//canvas.circle(5).attr({ cx: 100, cy: posicionesY }).fill('#ffffff');
+		canvas.text(notasArray[i]).move(30, posicionLetrasY).font({ fill: colorLetras[i], family: 'Inconsolata', size: 30 });
+
+		posicionLetrasY += 50;
+		//posicionesY += 50;
 	}
 
 
@@ -65,10 +88,68 @@ function pintaHeptagrama(canvas){
 /*
 	<circle cx="550" cy="50" r="20" fill="#022486" /><!-- nota B (si) y = 30 color azul-->
 <circle cx="550" cy="50" r="5" fill="#FFFFFF" />
-
 */
-	
 
+
+}
+
+
+
+
+
+
+
+
+function dibujaNota(canvas, linea, numero, semitono){
+	//linea = linea -1 ;
+	linea--;
+	var posucionX = 780; 
+	var colorNota = colorNotas(linea);
+	var posicionNotas = [50, 100, 150, 200, 250, 300, 350];
+
+	//var listaNotas = {b:0, B:0, a:1, A:1, c:6, C:6, d:5, D:5, e:4, E:4, f:3, F:3, g:2, G:2 }
+
+	canvas.circle(30).attr({ cx: posucionX+8, cy: posicionNotas[linea] }).fill(colorNota);
+	canvas.text(numero).move(posucionX, posicionNotas[linea]-22).font({ fill: "#ffffff", family: 'Inconsolata', size: 30});
+
+	if(semitono){
+		canvas.text("#/b").move(posucionX, posicionNotas[linea]-25).font({ fill: "#ffffff", family: 'Inconsolata', size: 10});
+	}
+
+
+
+
+/*
+canvas.circle(30).attr({ cx: 198, cy: 50 }).fill("#022486");
+canvas.text("4").move(190, 33).font({ fill: "#ffffff", family: 'Inconsolata'});
+//canvas.circle(5).attr({ cx: 198, cy: 50 }).fill("#b3b300");
+*/
+
+
+
+}
+
+
+
+
+
+
+
+function colorNotas(linea){
+
+	var coloresNota = ["#022486", "#402284", "#02948A", "#00A900", "#FFEE00", "#FF491A", "#F50417"];
+	return coloresNota[linea];
+
+}
+
+
+
+
+
+
+
+
+function moverNotas(){
 
 
 }
