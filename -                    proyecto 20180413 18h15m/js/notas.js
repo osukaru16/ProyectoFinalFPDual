7b2bf28c57f43e6ce12bde26013a9b77e249faa4
osukaru16 <osukaru16@gmail.com>
2018-04-16 18:20:28 +0200

    Add files via upload

---
+++ -                    proyecto 20180413 18h15m/js/notas.js
@@ -0,0 +1,25 @@
+/*
+Nota: Esta clase se a hace pensando el heptagrama las nota y posiciones se crearan en referencia. 
+
+
+*/
+
+
+
+
+function Notas(){
+
+	this.linea = 0;
+
+	this.coloresNota = ["#022486", "#402284", "#02948A", "#00A900", "#FFEE00", "#FF491A", "#F50417"];
+
+	this.coloresTexto = ["#b30000", "#b30000", "#b30000", "#ffffff", "#022486", "#022486", "#022486"];
+}
+
+
+Notas.prototype.pintaNota = function(){
+
+
+
+
+}
