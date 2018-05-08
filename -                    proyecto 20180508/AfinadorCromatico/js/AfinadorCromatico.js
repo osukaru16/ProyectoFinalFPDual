ba5a4c61fb80020d775e38e42739f6ade8fefad1
osukaru16 <osukaru16@gmail.com>
2018-05-08 17:47:43 +0200

    Add files via upload

---
+++ -                    proyecto 20180508/AfinadorCromatico/js/AfinadorCromatico.js
@@ -0,0 +1,437 @@
+
+
+
+
+
+
+
+function AfinadorCromatico(){
+
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+/*
+	this.audioContext = null;
+	
+	
+	this.analyser = null;
+*/	
+
+
+	//this.mediaStreamSource = null;
+	
+
+	//this.pitchElem;
+	//this.noteElem;
+
+
+	//this.audioContext = new AudioContext();
+
+
+	//this.analyser = this.audioContext.createAnalyser();
+
+
+	//this.arrayNotas = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+
+	this.frecuencia;
+
+	this.nota;
+
+	//this.test = "Neko1";
+
+
+
+
+}
+
+AfinadorCromatico.prototype.getFrecuenciaDetallada = function(){
+
+	//return this.frecuencia;
+
+	return frecuenciaDetallada;
+
+}
+
+
+AfinadorCromatico.prototype.getFrecuencia = function(){
+
+	//return this.frecuencia;
+
+	return frecuencia;
+
+}
+
+
+AfinadorCromatico.prototype.getNota = function(){
+
+	//return this.nota;
+
+	return this.nota;
+
+}
+
+
+
+AfinadorCromatico.prototype.setFrecuencia = function(frecuencia){
+
+	this.frecuencia = frecuencia;
+
+}
+
+
+AfinadorCromatico.prototype.setNota = function(nota){
+
+	this.nota = nota;
+
+}
+
+
+
+
+AfinadorCromatico.prototype.error = function(){
+	alert('Stream generation error.');
+
+}
+
+
+
+
+AfinadorCromatico.prototype.iniciarAfinador = function(){
+
+	this.audioContext = new AudioContext();
+	//this.test = "Neko2";
+
+	//alert(this.gotStream);
+
+
+    this.getUserMedia(
+    	{
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, iniciarStream);
+
+	
+//this.gotStream
+/*
+
+	   this.getUserMedia(
+    	{
+            "audio": true,
+                "optional": []
+            
+        }, this.gotStream);
+*/
+	//alert(this.test);
+
+}
+
+
+
+
+AfinadorCromatico.prototype.getUserMedia = function(dictionary, callback) {
+	//alert(this.test);
+    try {
+    	//this.test = "Neko3";
+        navigator.getUserMedia = navigator.getUserMedia ||
+        						 navigator.webkitGetUserMedia ||
+        						 navigator.mediaDevices.getUserMedia;
+        						 navigator.getUserMedia(dictionary, callback, this.error);
+
+        //alert(this.test);
+
+
+    } catch (e) {
+    	//this.test = "Neko4";
+        alert('getUserMedia threw exception :' + e);
+    }
+    //alert(this.test);
+}
+
+
+
+/*
+AfinadorCromatico.prototype.actualizarAfinador = function() {
+
+	 return actualizarAfinador();
+
+}
+
+*/
+
+
+
+
+
+/*
+	 Aqui sereinicia y pierden el valor declarado las variable y los metodos
+	 por eso lo hare con funciones
+
+*/
+
+
+
+
+
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+	var audioContext = null;
+	
+	
+	var analyser = null;
+
+
+	var frecuencia;
+
+	var nota;
+
+	var frecuenciaDetallada;
+
+
+
+
+
+function iniciarStream(stream) {
+
+	//this.neko();
+	//alert(stream);
+
+	//alert(this.test);
+
+	audioContext = new AudioContext();
+   
+    var mediaStreamSource = this.audioContext.createMediaStreamSource(stream);
+
+    
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+
+    mediaStreamSource.connect( analyser );
+
+
+    actualizarAfinador();
+
+
+    //this.neko();
+
+}
+
+/*
+AfinadorCromatico.prototype.neko = function() {
+
+	alert("neko");
+
+}
+*/
+
+/*
+Nota: tengo que revisar esta formula que creo 
+	  que el hecho de redondear hace que fallen las notas A0, C8 y creo que otras notas tambien fallan
+
+
+Mirar para que son estas formula:
+	Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+
+	440 * Math.pow(2,(note-69)/12);
+
+*/
+function sacarNotaPorFrecuencia(frecuencia) {
+
+	var listaNotas = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+	var noteNum = 12 * (Math.log( frecuencia / 440 )/Math.log(2) );
+
+	//return Math.round( noteNum ) + 69;
+	return listaNotas[(Math.round( noteNum ) + 69)%12];
+
+}
+
+
+
+
+
+
+
+
+
+function crearAfinador( buffer, sampleRate ) {
+
+	var GOOD_ENOUGH_CORRELATION = 0.9;
+	var SIZE = buffer.length;
+	var MAX_SAMPLES = Math.floor(SIZE/2);
+	var best_offset = -1;
+	var best_correlation = 0;
+
+
+	
+	var rms = 0;
+	/*
+	rms es la potencia real. siglas RMS (Root Mean Square) en la musica.
+	*/
+
+
+
+	var foundGoodCorrelation = false;
+	var correlations = new Array(MAX_SAMPLES);
+
+	for (var i=0;i<SIZE;i++) {
+		var val = buffer[i];
+		rms += val*val;
+	}
+
+
+	
+
+	rms = Math.sqrt(rms/SIZE);
+
+
+
+
+
+
+	if (rms<0.01) // esdto es para evitar un poco de ruido
+		return -1;
+
+	var lastCorrelation=1;
+	for (var offset = 0; offset < MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<MAX_SAMPLES; i++) {
+			correlation += Math.abs((buffer[i])-(buffer[i+offset]));
+		}
+		correlation = 1 - (correlation/MAX_SAMPLES);
+		correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+		if ((correlation>GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
+			foundGoodCorrelation = true;
+			if (correlation > best_correlation) {
+				best_correlation = correlation;
+				best_offset = offset;
+			}
+		} else if (foundGoodCorrelation) {
+
+				var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
+			return sampleRate/(best_offset+(8*shift));
+
+		}
+		lastCorrelation = correlation;
+	}
+	if (best_correlation > 0.01) {
+
+		return sampleRate/best_offset;
+	}
+
+
+
+	return -1;
+
+
+	/*
+	Nota en este punto es muy probablen que se el optimo para poner codigo para que detecte la nota A0 y similares.
+
+
+
+	*/
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function actualizarAfinador() {
+
+
+
+	
+
+	//var listaNotas = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+
+	var buffer = new Float32Array( 1024 );
+	analyser.getFloatTimeDomainData( buffer );
+	var afinador = crearAfinador( buffer, audioContext.sampleRate );
+
+	//alert(afinador);
+
+	if (afinador != -1) {
+
+		frecuencia = Math.round( afinador ) ;
+
+		frecuenciaDetallada = afinador;
+
+		nota =  sacarNotaPorFrecuencia( afinador );
+
+		//nota = listaNotas[nota%12];
+
+		//alert(this.sacarNotaPorFrecuencia( afinador ));
+
+
+		/*
+
+		// Nota: mirar que hace este codigo
+
+		if (!window.requestAnimationFrame)
+			window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+		rafID = window.requestAnimationFrame( actualizarAfinador );
+		*/
+
+		
+	}else{
+		frecuencia = 0;
+		nota = "0";
+	}
+
+
+
+	//var afinador = new AfinadorCromatico();
+
+	//afinador.setFrecuencia(frecuencia);
+	//afinador.setNota(nota);
+
+
+
+	document.getElementById("afinador").innerHTML = nota; //Esto hay que repararlo
+
+
+/*
+	var afinador = new AfinadorCromatico();
+	afinador.setNota(nota);
+*/
+
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	var rafID = window.requestAnimationFrame( actualizarAfinador );
+
+
+
+
+	
+
+
+}
+
+
+
+
+
+
---
+++ -                    proyecto 20180508/AfinadorCromatico/js/afinadorCromatico_v2.js
@@ -0,0 +1,367 @@
+
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioContext = null;
+var isPlaying = false;
+var sourceNode = null;
+var analyser = null;
+//var theBuffer = null;
+//var DEBUGCANVAS = null;
+var mediaStreamSource = null;
+var detectorElem, 
+	//canvasElem,
+	//waveCanvas,
+	pitchElem,
+	noteElem,
+	detuneElem,
+	detuneAmount;
+
+
+var test = "Neko1";
+
+window.onload = function() {
+	//audioContext = new AudioContext();
+	
+	detectorElem = document.getElementById( "detector" );
+
+	
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+
+
+	detuneElem = document.getElementById( "detune" );
+	detuneAmount = document.getElementById( "detune_amt" );
+
+}
+
+/*
+ function onloadAudioContext(){
+	audioContext = new AudioContext();
+ }
+*/
+
+
+
+function error() {
+    alert('Stream generation failed.');
+}
+
+
+
+
+//Nota: toggleLiveInput lo usa el boton
+
+function toggleLiveInput() {
+	//onloadAudioContext();
+	//audioContext = new AudioContext();
+
+	//test = "Neko2";
+
+	/*
+
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+    }
+
+    */
+alert(gotStream);
+    
+    getUserMedia(
+    	{
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream);
+
+    
+
+
+/*
+        getUserMedia(
+    	{
+            "audio": true,
+                "optional": []
+            
+        }, gotStream);
+
+*/
+
+
+
+/*
+    getUserMedia(
+    	{
+            "audio": {
+        		"optional": [
+                    {"googEchoCancellation": "false"},
+                    {"googAutoGainControl": "false"},
+                    {"googNoiseSuppression": "false"},
+                    {"googHighpassFilter": "false"}]
+                }}, gotStream);
+*/
+
+
+
+
+
+
+
+
+}
+
+
+
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+
+        //alert(stream);
+
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+
+
+
+
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    //alert(stream);
+	audioContext = new AudioContext();
+
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
+
+
+
+
+
+
+var rafID = null;
+var tracks = null;
+//var buflen = 1024;
+var buf = new Float32Array( 1024 );
+
+var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+
+
+
+function noteFromPitch( frequency ) {
+	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+	return Math.round( noteNum ) + 69;
+}
+
+
+
+function frequencyFromNoteNumber( note ) {
+	return 440 * Math.pow(2,(note-69)/12);
+}
+
+
+function centsOffFromPitch( frequency, note ) {
+	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+}
+
+
+
+
+
+
+
+
+
+function autoCorrelate( buf, sampleRate ) {
+	var SIZE = buf.length;
+	var MAX_SAMPLES = Math.floor(SIZE/2);
+	//alert(MAX_SAMPLES);
+	var best_offset = -1;
+	var best_correlation = 0;
+
+	var rms = 0;
+	/*
+	rms es la potencia real. siglas RMS (Root Mean Square) en la musica.
+	*/
+
+	var foundGoodCorrelation = false;
+	var correlations = new Array(MAX_SAMPLES);
+
+
+
+
+
+
+	for (var i=0;i<SIZE;i++) {
+		var val = buf[i];
+		rms += val*val;
+	}
+
+
+
+
+	rms = Math.sqrt(rms/SIZE);
+	//alert(rms);
+
+
+
+
+	if (rms<0.01) // not enough signal
+		return -1;
+
+	var lastCorrelation=1;
+	for (var offset = 0; offset < MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<MAX_SAMPLES; i++) {
+			correlation += Math.abs((buf[i])-(buf[i+offset]));
+		}
+		correlation = 1 - (correlation/MAX_SAMPLES);
+		correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+		if ((correlation>GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
+			foundGoodCorrelation = true;
+			if (correlation > best_correlation) {
+				best_correlation = correlation;
+				best_offset = offset;
+			}
+		} else if (foundGoodCorrelation) {
+			// short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+			// Now we need to tweak the offset - by interpolating between the values to the left and right of the
+			// best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
+			// we need to do a curve fit on correlations[] around best_offset in order to better determine precise
+			// (anti-aliased) offset.
+
+			// we know best_offset >=1, 
+			// since foundGoodCorrelation cannot go to true until the second pass (offset=1), and 
+			// we can't drop into this clause until the following pass (else if).
+			var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
+			return sampleRate/(best_offset+(8*shift));
+		}
+		lastCorrelation = correlation;
+	}
+	if (best_correlation > 0.01) {
+		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+		return sampleRate/best_offset;
+	}
+
+
+
+	return -1;
+	/*
+	Nota en este punto es muy probablen que se el optimo para poner codigo para que detecte la nota A0 y similares.
+
+
+
+	*/
+
+
+
+//	var best_frequency = sampleRate/best_offset;
+}
+
+
+
+
+
+
+
+
+
+
+function updatePitch() {
+	//var cycles = new Array;
+	analyser.getFloatTimeDomainData( buf );
+	var ac = autoCorrelate( buf, audioContext.sampleRate );
+	// TODO: Paint confidence meter on canvasElem here.
+
+ 	if (ac == -1) {
+
+ 		detectorElem.className = "vague";
+	 	pitchElem.innerText = "--";
+		noteElem.innerText = "-";
+		detuneElem.className = "";
+		detuneAmount.innerText = "--";
+		
+ 	} else {
+
+
+	 	detectorElem.className = "confident";
+	 	pitch = ac;
+	 	pitchElem.innerText = Math.round( pitch ) ;
+	 	var note =  noteFromPitch( pitch );
+	 	//alert(note);
+		noteElem.innerHTML = noteStrings[note%12];
+		//alert(note%12);
+	
+
+	}
+
+
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	rafID = window.requestAnimationFrame( updatePitch );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
+var GOOD_ENOUGH_CORRELATION = 0.9; // this is the "bar" for how close a correlation needs to be
+
+
+
+
+
+
